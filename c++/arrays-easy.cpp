d9c8e547e79cf7c16ddf0ec86bef94535f0d2450
vincentc48 <vincent.cai48@gmail.com>
2021-06-10 08:28:31 -0400

    Plus One in C++

--- c++/arrays-easy.cpp
+++ c++/arrays-easy.cpp
@@ -1,5 +1,6 @@
 class Solutions{
     public:
+        //Remove Duplicates
         int removeDuplicates(vector<int>& nums) {
             int l = 0;
             if(nums.size()==0) return 0;
@@ -12,6 +13,21 @@ class Solutions{
                 prevInt = nums[i];
             }
             return nums.size();
+        }
+
+        //Plus One
+        vector<int> plusOne(vector<int>& digits) {
+            int i = digits.size()-1;
+            do{
+                digits[i] = digits[i]+1;
+                if(i<digits.size()-1) digits[i+1] = 0;
+                i--;
+            }while(digits[i+1]>=10&&i>=0);
+            if(digits[0]==10){
+                digits[0] = 0;
+                digits.insert(digits.begin(),1);
+            }
+            return digits;
         }
         
 };
